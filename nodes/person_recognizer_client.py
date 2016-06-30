37b167ac395ca3c32da23ef09481d8b6fb2ad2e6
kendemu <kendemu@me.com>
2016-06-30 21:36:47 +0900

    add 3d

--- nodes/person_recognizer_client.py
+++ nodes/person_recognizer_client.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String, Int32, Float32
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, Imu
 from person_recognizer.msg import Gender, Face, Person
 from geometry_msgs.msg import Twist, Vector3
 from futaba_driver.msg import Servo
+from object_recognizer.msg import Person 
 from subprocess import call
 import numpy as np
 import time
 import os
 import math
+import thread
 
 path = "/home/kendemu/catkin_ws/src/person_recognizer"
 
@@ -22,12 +24,25 @@ vel_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10
         
 person_info_received = False
 
-
 class PersonRecognizerClient:
     person_info = Person()
     name = ""
     state = 0
+    speed = 0
+    current_angle = 0
+    serif = 0
+    num_people = 0
 
+    @staticmethod
+    def ImuCb(data):
+        if abs(data.angular_velocity.z) > 0.01:
+            PersonRecognizerClient.speed = data.angular_velocity.z
+        else:
+             PersonRecognizerClient.speed = 0.0
+
+    @staticmethod
+    def locCb(data):
+       PersonRecognizerClient.num_people = len(data.centroids)
 
     @staticmethod
     def speak(text):
@@ -65,65 +80,112 @@ class PersonRecognizerClient:
         if PersonRecognizerClient.state == 0:
             self.speak("Operator, I am going to tell you 3 things.")
             self.speak("1st")
-            self.speak("Operator, tell your name by this sentence only.")
+            self.speak("Operator, tell your name")
+            self.speak("by this sentence only.")
             self.speak("I am Bob, I am John")
             self.speak("2nd")
             self.speak("Operator, go to the front of Mini Robot.")
+            self.speak("Make your face straight")
             self.speak("3rd")
-            self.speak("Operator, say it in a loud voice.")
+            self.speak("Very important thing.")
+            self.speak("Operator, say it loud as you can.")
+            self.speak("Operator, say it loud as you can.")
+            self.speak("Operator, say it loud as you can.")
             self.speak("OK?")
             PersonRecognizerClient.state = 1
 
         elif PersonRecognizerClient.state == 1:
             self.speak("I am going to remember you.")
             self.speak("Say your name")
+            self.speak("3")
+            self.speak("2")
+            self.speak("1")
+            self.speak("go")
             speech_control_pub.publish("speak")
             time.sleep(3)
             PersonRecognizerClient.state = 2
 
         elif PersonRecognizerClient.state == 2:
-            time.sleep(1)
             if PersonRecognizerClient.name is "":
-                self.speak("I am hearing you")
-                self.speak("Please say it in a loud voice")
+                self.speak("Waiting for operator talking")
             else:
-                self.speak("your name is " + PersonRecognizerClient.name)
+                rospy.loginfo("serif : %d", PersonRecognizerClient.serif)
+                if PersonRecognizerClient.serif is 1 or PersonRecognizerClient.serif is 0:
+                    self.speak("Make your face straight")
+                elif PersonRecognizerClient.serif== 2:
+                    self.speak("going near you")
+                    vel = Twist()
+                    vel.linear.x = 1.0
+                    vel_pub.publish(vel)
+                    time.sleep(0.5)
+                elif PersonRecognizerClient.serif == 3:
+                    self.speak("turning to find your face")
+                    vel = Twist()
+                    vel.angular.z = -1.0
+                    vel_pub.publish(vel)
+                    time.sleep(0.5)
+
+                elif PersonRecognizerClient.serif % 2 == 0:
+                    self.speak("go near me")
+                    vel = Twist()
+                    vel.angular.z = 1.0
+                    vel_pub.publish(vel)
+                    time.sleep(0.5)
+                    vel_pub.publish(vel)
+
+                elif PersonRecognizerClient.serif % 2 == 1:
+                    self.speak("turning to find your face")
+                    vel = Twist()
+                    vel.angular.z = -1.0
+                    vel_pub.publish(vel)
+                    time.sleep(0.5)
+                    vel_pub.publish(vel)
+
+                else:
+                    self.speak("your name is " + PersonRecognizerClient.name)
                 print "faces number : ", len(data.faces)
                 if len(data.faces) > 0:
                     self.remembered_face = data.faces[np.argmin(map(lambda x : x,data.xangle))]
                     person_recog_pub.publish(self.remembered_face)
                     speech_control_pub.publish("stop")
                     person_state_pub.publish(1)
-                    time.sleep(1)
+                    #time.sleep(1)
                     PersonRecognizerClient.state = 3 
                 else:
-                    self.speak("I am finding you")
-                    self.speak("Go front of minis")
-                    self.speak("look at mini eye.")
-
-
+                    PersonRecognizerClient.serif += 1
+                    
         elif PersonRecognizerClient.state == 3:
             self.speak("waiting 10 seconds")
             time.sleep(10)
             PersonRecognizerClient.state = 4
+            self.speak("finding operator")
+            self.speak("first turning")
 
         elif PersonRecognizerClient.state == 4:
-            self.speak("finding operator")
-            self.speak("first turning") 
-            #PersonRecognizerClient.name
-            vel = Twist()
-            vel.angular.z = 3.14
-            vel_pub.publish(vel)
-            time.sleep(1.0)
-            vel_pub.publish(vel)
-            time.sleep(1.0)
-            vel.angular.z = 1.0
-            vel_pub.publish(vel)
-            time.sleep(1.0)
-            PersonRecognizerClient.state = 5
+                vel = Twist()
+                vel.angular.z = 3.14
+                vel_pub.publish(vel)
+                time.sleep(1.0)
+                vel_pub.publish(vel)
+                time.sleep(1.0)
+                vel.angular.z = 1.4
+                vel_pub.publish(vel)
+                time.sleep(1.0)
+                vel = Twist()
+                vel.linear.x = 1.0
+                vel_pub.publish(vel)
+                time.sleep(1.0)
+                
+                vel_pub.publish(vel)
+                PersonRecognizerClient.state = 5
         
         elif PersonRecognizerClient.state == 5:
             self.speak("finding operator")
+            self.speak("Everyone")
+            self.speak("Make your face straight")
+            self.speak("Make your face straight")
+            self.speak("Make your face straight")
+
             self.speak("scanning faces")
             person_state_pub.publish(2)
             time.sleep(1)
@@ -136,46 +198,75 @@ class PersonRecognizerClient:
                     print "person_info_received", person_info_received
                     self.person_index  = np.argmin(PersonRecognizerClient.person_info.distance)
                     print "face location", self.person_index
-                    for j in range(2):
-                        self.speak("operator "+ PersonRecognizerClient.name + " relative position from Happy Mini")
 
+                    self.speak("operator "+ PersonRecognizerClient.name + " relative position from Happy Mini")
+                        
                     self.speak("at azimuth angle " + str(PersonRecognizerClient.person_info.xangle[self.person_index]))
-                    self.speak("at elevation angle " + str(-PersonRecognizerClient.person_info.yangle[self.person_index]))
+                    twist_angle = -PersonRecognizerClient.person_info.xangle[self.person_index]
+                    self.speak("at elevation angle " + str(-PersonRecognizerClient.person_info.yangle[self.person_index]/2))
                     self.speak("operator " + PersonRecognizerClient.name + " gender is " + PersonRecognizerClient.person_info.gender[self.person_index])
 
                     self.speak("I am going to point you with my arm")
                     self.speak("and my direction")
-                    servo = Servo()
-                    servo.idx    = [i+1 for i in range(6)]
-                    servo.torque = [1, 1, 1, 1, 1, 0]
-                    servo.angle = [-PersonRecognizerClient.person_info.yangle[self.person_index], -PersonRecognizerClient.person_info.yangle[self.person_index] , -90, 90, 15, 0]
 
                     vel = Twist()
-                    vel.angular.z = math.radians(PersonRecognizerClient.person_info.xangle[self.person_index])
-                    servo.time = [1 for i in range(6)]
-                    servo.parent_frame = ["base_link" for i in range(6)]
-                    arm_pub.publish(servo)
-                    time.sleep(5.0)
+                    vel.angular.z = math.radians(twist_angle)
                     vel_pub.publish(vel)
-                    self.speak("moved arm")
+
+                    servo = Servo()
+                    servo.time = [3 for i in range(6)]
+                    servo.idx    = [i+1 for i in range(6)]
+                    servo.torque = [1, 1, 1, 1, 1, 1]
+                    servo.angle = [-PersonRecognizerClient.person_info.yangle[self.person_index]/2 + 140, -PersonRecognizerClient.person_info.yangle[self.person_index]/2 + 140, -140, 140, 15, 0]
+                    arm_pub.publish(servo)
+                    time.sleep(1.0)
+                    arm_pub.publish(servo)
+                    time.sleep(1.0)
+                    arm_pub.publish(servo)
+                    time.sleep(1.0)
+                    rospy.loginfo("moving arm")
+
+                    self.speak("Happy mini pointed you, " + PersonRecognizerClient.name)
                     time.sleep(1.0)
 
             else:
-                self.speak("Waiting for Finding Operator")
+                self.speak("Waiting for Finding Operator" + PersonRecognizerClient.name)
 
         elif PersonRecognizerClient.state == 7:
-            for j in range(2):
-                self.speak("the number of human is ")
-                self.speak(str(len(PersonRecognizerClient.person_info.faces)))
 
             if person_info_received is True:
+                women_num = 0
+                men_num = 0
+                sitting_num = 0
+                standing_num = 0
+
                 for i in range(len(PersonRecognizerClient.person_info.faces)):
-                    self.speak("person " + str(i))
-                    self.speak("person " + str(i) + " location from the relative position from Happy Mini")
-                    self.speak("at azimuth angle "+str(PersonRecognizerClient.person_info.xangle[i]))
-                    self.speak("at elevation angle "+str(-PersonRecognizerClient.person_info.yangle[i]))
-                    self.speak("the gender is " + PersonRecognizerClient.person_info.gender[i])
-                PersonRecognizerClient.state = 8
+                    if PersonRecognizerClient.person_info.gender[i] == "man":
+                        men_num += 1
+                    else:
+                        women_num += 1
+                    if PersonRecognizerClient.person_info.yangle[i] > 0:
+                        sitting_num += 1
+                    else:
+                        standing_num += 1
+
+                if len(PersonRecognizerClient.person_info.faces) > 0:
+                    self.speak("the number of human is ")
+                    self.speak(str(PersonRecognizerClient.num_people))
+                    self.speak("number of man is " +str(men_num))
+                    self.speak("number of woman is " + str(women_num))
+                    self.speak("number of people undentified is " + str(PersonRecognizerClient.num_people - men_num - women_num))
+                    self.speak("number of people sitting is " + str(sitting_num))
+                    self.speak("number of people standing is " +str(standing_num))
+                    PersonRecognizerClient.state = 8
+
+                else:
+                    self.speak("finding people")
+                    vel = Twist()
+                    vel.linear.z = 0.5
+                    vel_pub.publish(vel)
+                    time.sleep(1.0)
+
             else:
                 rospy.loginfo("waiting for recognition result")
 
@@ -192,5 +283,6 @@ if __name__ == "__main__":
     person_rec_cli = PersonRecognizerClient()
     person_sub = rospy.Subscriber("/person", Person, person_rec_cli.setPersonInfo)
     name_sub = rospy.Subscriber("/name",String, person_rec_cli.setName)
-
+    imu_sub = rospy.Subscriber("/mobile_base/sensors/imu_data_raw", Imu, person_rec_cli.ImuCb)
+    num_sub = rospy.Subscriber("/human_location", Person, person_rec_cli.locCb)
     rospy.spin()
